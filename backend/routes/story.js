0d2bf700f4e747ae79673f56bf3c91eaea78baed
Shresth Pratyush <shresthpratyush@Shresths-MacBook-Air.local>
2025-03-29 14:50:33 +0530

    Added story routes

---
+++ backend/routes/story.js
@@ -0,0 +1,112 @@
+import express from "express";
+import { authMiddleware } from "../middleware.js";
+import { Story } from "../db.js";
+import cloudinary from "cloudinary";
+
+const router = express.Router();
+
+const uploadToCloudinary = async (file) => {
+    try {
+        const result = await cloudinary.uploader.upload(file.tempFilePath, {
+            resource_type: "auto",
+        });
+        return result.secure_url;
+    } catch (error) {
+        console.log("Error in uploadToCloudinary", error);
+        throw new Error("Error uploading to Cloudinary");
+    }
+};
+router.post("/post", authMiddleware, async (req, res) => {
+    try {
+        if (!req.files || !req.files.imageFile) {
+            return res.status(400).json({ message: "Upload all files" });
+        }
+        const imageFile = req.files.imageFile;
+        const imageUrl = await uploadToCloudinary(imageFile);
+        const { title, description, content, likes, genre } = req.body;
+        const authorId = req.userId;
+        const story = await Story.create({
+            title,
+            imageUrl,
+            description,
+            content,
+            likes,
+            authorId,
+            genre,
+        });
+
+        return res.status(201).json(story);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ message: "Error while uploading story" });
+    }
+});
+
+router.get("/all", authMiddleware, async (req, res) => {
+    try {
+        const stories = await Story.find();
+        return res.status(200).json(stories);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ message: "Error while fetching stories" });
+    }
+});
+
+router.get("/story/:id", authMiddleware,async (req, res) => {
+    try {
+        const { id } = req.params;
+        const story = await Story.findById(id);
+
+        if (!story) {
+            return res.status(404).json({ message: "Story not found" });
+        }
+
+        return res.status(200).json(story);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ message: "Error while fetching story" });
+    }
+});
+
+router.get("/most-liked", authMiddleware,async (req, res) => {
+    try {
+        const stories = await Story.find().sort({ likes: -1 }).limit(10);
+        return res.status(200).json(stories);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ message: "Error while fetching most-liked stories" });
+    }
+});
+router.get("/genre",authMiddleware, async (req, res) => {
+    try {
+        const { name } = req.query; 
+        if (!name) {
+            return res.status(400).json({ message: "Genre is required" });
+        }
+        const stories = await Story.find({ genre: name });
+        if (stories.length === 0) {
+            return res.status(404).json({ message: "No stories found for this genre" });
+        }
+        return res.status(200).json(stories);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ message: "Error while fetching stories of this genre" });
+    }
+});
+
+router.get("/story", authMiddleware, async (req, res) => {
+    try {
+        const userId = req.userId;
+
+        const stories = await Story.find({ authorId: userId });
+
+        return res.status(200).json(stories);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ message: "Error while fetching stories of this user" });
+    }
+});
+
+
+
+export default router;

5a7d1b140633b4464472df762ba0a96650f3b861
gupta-harshal <harshal.2023ug3013@iiitranchi.ac.in>
2025-03-29 16:16:44 +0530

    add changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { Book, Search, User, ChevronDown, Plus, Check, X, Clock } from 'lucide-r
 import { SectionCard } from './components/SectionCard';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from './Pages/Home';
+import { CreateStory } from './Pages/CreateStory';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
+      <Route path='/createStory' element={<CreateStory/>}></Route>
     </Routes>
     </BrowserRouter>
   );
--- frontend/src/Pages/CreateStory.jsx
+++ frontend/src/Pages/CreateStory.jsx
@@ -1,3 +1,15 @@
-export function CreateStory(){
+import { Book, User } from "lucide-react";
+import { Navbar } from "../components/navbar";
 
+export function CreateStory() {
+    return (
+        <div className='w-full h-screen' style={{
+                backgroundImage: "url('/Homebg.png')",
+                backgroundSize: 'cover',   
+              }}>
+            <Navbar />
+            <div className="flex justify-center flex-col mx-[10%] bg-background h-[40%] w-[40%] rounded-lg shadow-lg mx-auto p-8">
+            </div>
+        </div>
+    );
 };
